f39a2bf66922f23bd25f5b5c3c6c1163da2cdf1e
Mohamed Aboulmaaly <maboulmaaly@yahoo.com>
2021-05-19 23:11:29 +0100

    create views (create, update, show) and update post controller and post model

---
+++ resources/views/blog/edit.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="w-4/5 m-auto text-left">
+        <div class="py-15">
+            <h1 class="text-6xl">Update Post</h1>
+        </div>
+    </div>
+    @if ($errors->any())
+        <div class="m-auto w-4/5">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li class="mb-4 w-1/5 text-gray-50 bg-red-700 rounded-2xl py-4">
+                        {{ $error }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <div class="m-auto pt-20 w-4/5">
+        <form action="/blog/{{ $post->slug }}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <input type="text" name="title" value="{{ $post->title }}" class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            <textarea name="description" class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none">{{ $post->description }}</textarea>
+            {{-- <div class="bg-gray-lighter pt-15">
+                <label for="image" class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
+                    <span class="mt-2 text-base leading-normal">
+                        Select a file
+                    </span>
+                    <input type="file" id="image" name="image" class="hidden">
+                </label>
+            </div> --}}
+            <button type="submit" class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">Submit post</button>
+        </form>
+    </div>
+@endsection
